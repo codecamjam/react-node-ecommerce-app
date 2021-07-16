38058cc263eb80944379aa4b02e3f6b79d167231
codecamjam <camerontaghlabi@gmail.com>
2021-07-15 20:15:32 -0400

    l9 connecting to mongodb

--- ecommerce/app.js
+++ ecommerce/app.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
+//app
 const app = express();
 
+//db
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
+  .then(() => console.log('DB Connected'));
+
+//routes
 app.get('/', (req, res) => {
   res.send('hello from node');
 });
