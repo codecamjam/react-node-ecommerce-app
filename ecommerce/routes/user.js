5f364c5467ac3b7756946947e1d12d03e6772897
codecamjam <camerontaghlabi@gmail.com>
2021-07-15 20:27:44 -0400

    l11 controllers

--- ecommerce/routes/user.js
+++ ecommerce/routes/user.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('hello from node');
-});
+const { sayHi } = require('../controllers/user');
+
+router.get('/', sayHi);
 
 module.exports = router;
