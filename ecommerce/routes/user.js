b97baee7b413179942431b13baaf71a458fc0fe9
codecamjam <camerontaghlabi@gmail.com>
2021-07-15 22:19:44 -0400

    l22 user signout

--- ecommerce/routes/user.js
+++ ecommerce/routes/user.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { signup, signin } = require('../controllers/user');
+const { signup, signin, signout } = require('../controllers/user');
 const { userSignupValidator } = require('../validator');
 
 const router = express.Router();
 
 router.post('/signup', userSignupValidator, signup);
 router.post('/signin', signin);
+router.get('/signout', signout);
 
 module.exports = router;
