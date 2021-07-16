ede0b2157a8ff0bf9680c2731c070392531711f6
codecamjam <camerontaghlabi@gmail.com>
2021-07-16 19:15:40 -0400

    l34 product by id middleware and single product

--- ecommerce/controllers/product.js
+++ ecommerce/controllers/product.js
@@ -4,6 +4,26 @@ const formidable = require('formidable');
 const Product = require('../models/product');
 const { errorHandler } = require('../helpers/dbErrorHandler');
 
+exports.productById = (req, res, next, id) => {
+  Product.findById(id).exec((err, product) => {
+    if (err || !product) {
+      return res.status(400).json({
+        error: 'Product not found'
+      });
+    }
+
+    req.product = product;
+
+    next();
+  });
+};
+
+exports.read = (req, res) => {
+  req.product.photo = undefined;
+
+  return res.json(req.product);
+};
+
 exports.create = (req, res) => {
   const form = new formidable.IncomingForm();
   form.keepExtensions = true;
