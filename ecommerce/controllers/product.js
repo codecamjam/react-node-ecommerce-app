1a7975bc9b5eafb34882272ffce2171ebf2d3379
codecamjam <camerontaghlabi@gmail.com>
2021-07-16 19:00:29 -0400

    l33 create production validation

--- ecommerce/controllers/product.js
+++ ecommerce/controllers/product.js
@@ -14,9 +14,40 @@ exports.create = (req, res) => {
       });
     }
 
+    //check for all fields
+    const {
+      name,
+      description,
+      price,
+      category,
+      quantity,
+      shipping
+    } = fields;
+
+    if (
+      !name ||
+      !description ||
+      !price ||
+      !category ||
+      !quantity ||
+      !shipping
+    ) {
+      return res.status(400).json({
+        error: 'All fields are required'
+      });
+    }
+
     const product = new Product(fields);
 
     if (files.photo) {
+      // console.log('files photo', files.photo);
+
+      if (files.photo.size > 1000000) {
+        return res.status(400).json({
+          error: 'Image should be less than 1mb in size'
+        });
+      }
+
       product.photo.data = fs.readFileSync(files.photo.path);
       product.photo.contentType = files.photo.type;
     }
