8f377501f5f49c23c91d0f8e202cf4843e35a120
codecamjam <camerontaghlabi@gmail.com>
2021-07-20 13:34:02 -0400

    l112 show products in cart page

--- ecommerce-front/src/Routes.js
+++ ecommerce-front/src/Routes.js
@@ -10,6 +10,7 @@ import AddCategory from './admin/AddCategory';
 import AddProduct from './admin/AddProduct';
 import Shop from './core/Shop';
 import Product from './core/Product';
+import Cart from './core/Cart';
 
 const Routes = () => {
   return (
@@ -20,6 +21,7 @@ const Routes = () => {
         <Route path="/signin" exact component={Signin} />
         <Route path="/signup" exact component={Signup} />
         <Route path="/product/:productId" exact component={Product} />
+        <Route path="/cart" exact component={Cart} />
         <PrivateRoute path="/user/dashboard" exact component={Dashboard} />
         <AdminRoute
           path="/admin/dashboard"
