7b997e20ee3420cc9078f88f5144e1b9f6b64d6f
codecamjam <camerontaghlabi@gmail.com>
2021-07-20 12:25:43 -0400

    l107 product detail on single product page

--- ecommerce/controllers/product.js
+++ ecommerce/controllers/product.js
@@ -5,17 +5,19 @@ const Product = require('../models/product');
 const { errorHandler } = require('../helpers/dbErrorHandler');
 
 exports.productById = (req, res, next, id) => {
-  Product.findById(id).exec((err, product) => {
-    if (err || !product) {
-      return res.status(400).json({
-        error: 'Product not found'
-      });
-    }
+  Product.findById(id)
+    .populate('category')
+    .exec((err, product) => {
+      if (err || !product) {
+        return res.status(400).json({
+          error: 'Product not found'
+        });
+      }
 
-    req.product = product;
+      req.product = product;
 
-    next();
-  });
+      next();
+    });
 };
 
 exports.read = (req, res) => {
