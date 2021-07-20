a279959f314780f8a22b57215146232fadd5227d
codecamjam <camerontaghlabi@gmail.com>
2021-07-20 12:10:25 -0400

    l106 reuse card for single product

--- ecommerce-front/src/core/Product.js
+++ ecommerce-front/src/core/Product.js
@@ -1,7 +1,7 @@
 import Layout from './Layout';
 import { read } from './apiCore';
 import { useState, useEffect } from 'react';
-// import Card from './Card';
+import Card from './Card';
 // import Search from './Search';
 
 const Product = (props) => {
@@ -26,12 +26,19 @@ const Product = (props) => {
 
   return (
     <Layout
-      title="Home Page"
-      description="Node React E-commerce App"
+      title={product && product.name}
+      description={
+        product &&
+        product.description &&
+        product.description.substring(0, 100)
+      }
       className="container-fluid"
     >
-      <h2 className="mb-4">Single Product</h2>
-      <div className="row">{JSON.stringify(product)}</div>
+      <div className="row">
+        {product && product.description && (
+          <Card product={product} showViewProductButton={false} />
+        )}
+      </div>
     </Layout>
   );
 };
