df9d459c4a2ce983b42791297204b9d0265bf2ff
codecamjam <camerontaghlabi@gmail.com>
2021-07-21 17:25:09 -0400

    l143 user profile update

--- ecommerce-front/src/user/Profile.js
+++ ecommerce-front/src/user/Profile.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import Layout from '../core/Layout';
 import { isAuthenticated } from '../auth';
-import { Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import { read, update, updateUser } from './apiUser';
 
 const Profile = ({ match }) => {
@@ -28,6 +28,78 @@ const Profile = ({ match }) => {
     });
   };
 
+  const handleChange = (name) => (e) => {
+    setValues({ ...values, error: false, [name]: e.target.value });
+  };
+
+  const clickSubmit = (e) => {
+    e.preventDefault();
+    update(userId, token, { name, email, password }).then((data) => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        updateUser(data, () => {
+          setValues({
+            ...values,
+            name: data.name,
+            email: data.email,
+            success: true,
+          });
+        });
+      }
+    });
+  };
+
+  const redirectUser = (success) => {
+    if (success) {
+      return <Redirect to="/cart" />;
+    }
+  };
+
+  const profileUpdate = (name, email, password) => (
+    <form>
+      <div className="form-group">
+        <label htmlFor="" className="text-muted">
+          Name
+        </label>
+        <input
+          type="text"
+          onChange={handleChange('name')}
+          className="form-control"
+          value={name}
+        />
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="" className="text-muted">
+          Email
+        </label>
+        <input
+          type="email"
+          onChange={handleChange('email')}
+          className="form-control"
+          value={email}
+        />
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="" className="text-muted">
+          Password
+        </label>
+        <input
+          type="password"
+          onChange={handleChange('password')}
+          className="form-control"
+          value={password}
+        />
+      </div>
+
+      <button onClick={clickSubmit} className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
+
   useEffect(() => {
     init(userId);
     /*eslint-disable */
@@ -40,7 +112,8 @@ const Profile = ({ match }) => {
       className="container-fluid"
     >
       <h2 className="mb-4">Profile update</h2>
-      {JSON.stringify(values)}
+      {profileUpdate(name, email, password)}
+      {redirectUser(success)}
     </Layout>
   );
 };
