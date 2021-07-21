f7651922b42c98269d299cf3313feb8d4bacef26
codecamjam <camerontaghlabi@gmail.com>
2021-07-21 17:09:33 -0400

    l142 get user info for profile update

--- ecommerce-front/src/user/Profile.js
+++ ecommerce-front/src/user/Profile.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import Layout from '../core/Layout';
 import { isAuthenticated } from '../auth';
 import { Link } from 'react-router-dom';
-const Profile = () => {
-  return <div>profiel</div>;
+import { read, update, updateUser } from './apiUser';
+
+const Profile = ({ match }) => {
+  const [values, setValues] = useState({
+    name: '',
+    email: '',
+    password: '',
+    error: '',
+    success: false,
+  });
+
+  const { token } = isAuthenticated();
+  const { name, email, password, error, success } = values;
+  const { userId } = match.params;
+
+  const init = (userId) => {
+    // console.log(userId);
+    read(userId, token).then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: true });
+      } else {
+        setValues({ ...values, name: data.name, email: data.email });
+      }
+    });
+  };
+
+  useEffect(() => {
+    init(userId);
+    /*eslint-disable */
+  }, []);
+
+  return (
+    <Layout
+      title="Profile"
+      description="Update your profile"
+      className="container-fluid"
+    >
+      <h2 className="mb-4">Profile update</h2>
+      {JSON.stringify(values)}
+    </Layout>
+  );
 };
 
 export default Profile;
