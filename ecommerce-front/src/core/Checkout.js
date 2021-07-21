7dde4df832e6b2cc54f92b05d5a60602de9e1a3c
codecamjam <camerontaghlabi@gmail.com>
2021-07-21 13:16:14 -0400

    l131save delivery address of orders

--- ecommerce-front/src/core/Checkout.js
+++ ecommerce-front/src/core/Checkout.js
@@ -61,6 +61,8 @@ const Checkout = ({ products, setRun = (f) => f, run = undefined }) => {
     );
   };
 
+  let deliveryAddress = data.address;
+
   const buy = () => {
     setData({ loading: true });
     // send the nonce to your server
@@ -93,7 +95,7 @@ const Checkout = ({ products, setRun = (f) => f, run = undefined }) => {
               products: products,
               transaction_id: response.transaction.id,
               amount: response.transaction.amount,
-              address: data.address,
+              address: deliveryAddress,
             };
 
             createOrder(userId, token, createOrderData)
