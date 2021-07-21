f689223163ff14ba7fdc141127c1554820ccc158
codecamjam <camerontaghlabi@gmail.com>
2021-07-21 13:27:54 -0400

    132 push orders to user's purchase history

--- ecommerce/controllers/user.js
+++ ecommerce/controllers/user.js
@@ -37,3 +37,33 @@ exports.update = (req, res) => {
     }
   );
 };
+
+exports.addOrderToUserHistory = (req, res, next) => {
+  const history = [];
+
+  req.body.order.products.forEach(item => {
+    history.push({
+      _id: item._id,
+      name: item.name,
+      description: item.description,
+      category: item.category,
+      quantity: item.count,
+      transaction_id: req.body.order.transaction_id,
+      amount: req.body.order.amount
+    });
+  });
+
+  User.findOneAndUpdate(
+    { _id: req.profile._id },
+    { $push: { history } },
+    { new: true },
+    error => {
+      if (error) {
+        return res.status(400).json({
+          error: 'Could not update user purchase history'
+        });
+      }
+      next();
+    }
+  );
+};
--- ecommerce/routes/order.js
+++ ecommerce/routes/order.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const { requireSignin, isAuth } = require('../controllers/auth');
-const { userById } = require('../controllers/user');
+const { userById, addOrderToUserHistory } = require('../controllers/user');
 const { create } = require('../controllers/order');
 
 const router = express.Router();
 
-router.post('/order/create/:userId', requireSignin, isAuth, create);
+router.post(
+  '/order/create/:userId',
+  requireSignin,
+  isAuth,
+  addOrderToUserHistory,
+  create
+);
 
 router.param('userId', userById);
 
