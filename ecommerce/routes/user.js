90f6f7a124891f5c747e5443066bdebd799ef939
codecamjam <camerontaghlabi@gmail.com>
2021-07-22 09:35:49 -0400

    l44 user purchase history backend

--- ecommerce/routes/user.js
+++ ecommerce/routes/user.js
@@ -3,7 +3,12 @@ const express = require('express');
 const router = express.Router();
 
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
-const { userById, read, update } = require('../controllers/user');
+const {
+  userById,
+  read,
+  update,
+  purchaseHistory
+} = require('../controllers/user');
 
 router.get(
   '/secret/:userId',
@@ -17,6 +22,12 @@ router.get(
 
 router.get('/user/:userId', requireSignin, isAuth, read);
 router.put('/user/:userId', requireSignin, isAuth, update);
+router.get(
+  '/orders/by/user/:userId',
+  requireSignin,
+  isAuth,
+  purchaseHistory
+);
 
 router.param('userId', userById);
 
