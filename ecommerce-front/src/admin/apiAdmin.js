3263ad1e80003cfad293d7a27ebc7251032800f6
codecamjam <camerontaghlabi@gmail.com>
2021-07-22 10:56:11 -0400

    l150 gt all products for crud admin

--- ecommerce-front/src/admin/apiAdmin.js
+++ ecommerce-front/src/admin/apiAdmin.js
@@ -88,7 +88,7 @@ export const updateOrderStatus = (userId, token, orderId, status) => {
  * delete single product
  */
 export const getProducts = () => {
-  return fetch(`${API}/products`, {
+  return fetch(`${API}/products?limit=${100}`, {
     method: 'GET',
   })
     .then((response) => {
